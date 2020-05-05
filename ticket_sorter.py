fad44f58c2e6d1def5fd18043c1828332e1eb468
Thomas <thomas.meche@gmail.com>
2020-05-05 10:12:09 -0600

    5-5 Commit :     - compressed days list int list comp     - renamed cl1 to client_data     - added newline to client input

--- ticket_sorter.py
+++ ticket_sorter.py
@@ -10,39 +10,30 @@ TODO: distribution
     - Package as executable
 '''
 import pandas as pd
-import os
 from pathlib import Path
 
-client = input('Please specify a client: ')
+client = input('Please specify a client: \n')
 month = input('Please enter the month: \n')
 file_path = input('Input file path: \n').strip('"')
 
 file_list = Path(file_path).glob('*.xlsx')
 
+
 with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
     for file in file_list:
         with pd.ExcelFile(file) as xls:
-            sun = pd.read_excel(xls, 0)
-            mon = pd.read_excel(xls, 1)
-            tue = pd.read_excel(xls, 2)
-            wed = pd.read_excel(xls, 3)
-            thu = pd.read_excel(xls, 4)
-            fri = pd.read_excel(xls, 5)
-            sat = pd.read_excel(xls, 6)
-
-            days = [sun, mon, tue, wed, thu, fri, sat]
-
+            days = [pd.read_excel(xls,day) for day in range(7)]
             # turn this into a function
             for day in days:
+
                 try:
                     day = day[['Customer', 'Ticket Number/Action:', 'Time Worked:']].dropna()
                     if not day.empty:
-                        cl1 = day['Customer'].str.contains(client.title())
+                        client_data = day['Customer'].str.contains(client.title())
                         ticket_data.write(
-                            f'{client.upper()} \n' + day.loc[cl1].to_string(
+                            f'{client.upper()}\n' + day.loc[client_data].to_string(
                                 columns=['Ticket Number/Action:'], index=False, header=False
                             ) + '\n\n')
-
                 except AttributeError:
                     pass
 
