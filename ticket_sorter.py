e5aab6c5680360676f27fc50a1febb0efb179dff
Thomas <thomas.meche@gmail.com>
2020-05-06 09:39:24 -0600

    5-6 Commit :     - can now output total hours per day per client     - added time module to benchmark time of actual function (For fun)     - added start and end times to df for calculations     - made names more pandas friendly using df.rename()     - added day.worked calculations line 49

--- ticket_sorter.py
+++ ticket_sorter.py
@@ -11,10 +11,14 @@ TODO: distribution
 '''
 import pandas as pd
 from pathlib import Path
+import time
+
 
 client = input('Please specify a client: \n')
 month = input('Please enter the month: \n')
 file_path = input('Input file path: \n').strip('"')
+
+timer_start = time.perf_counter()
 
 file_list = Path(file_path).glob('*.xlsx')
 
@@ -22,19 +26,38 @@ file_list = Path(file_path).glob('*.xlsx')
 with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
     for file in file_list:
         with pd.ExcelFile(file) as xls:
-            days = [pd.read_excel(xls,day) for day in range(7)]
-            # turn this into a function
+            days = [pd.read_excel(xls, day, parse_dates=['Start Time:', 'End Time:', 'Time Worked:'])
+                    for day in range(7)]
+
             for day in days:
 
                 try:
-                    day = day[['Customer', 'Ticket Number/Action:', 'Time Worked:']].dropna()
+                    day = day[[
+                        'Start Time:',
+                        'End Time:',
+                        'Customer',
+                        'Ticket Number/Action:',
+                        'Time Worked:']].dropna()
+
+                    day.rename(columns={
+                        'Start Time:': 'start',
+                        'End Time:': 'end',
+                        'Ticket Number/Action:': 'tickets',
+                        'Time Worked:': 'worked'
+                    }, inplace=True)
+
+                    day.worked = day.end - day.start
+
                     if not day.empty:
                         client_data = day['Customer'].str.contains(client.title())
                         ticket_data.write(
-                            f'{client.upper()}\n' + day.loc[client_data].to_string(
-                                columns=['Ticket Number/Action:'], index=False, header=False
+                            f'{client.upper()} {day.worked.sum()} \n' + day.loc[client_data].to_string(
+                                columns=['tickets'], index=False, header=False
                             ) + '\n\n')
+
                 except AttributeError:
                     pass
+timer_finish = time.perf_counter()
 
-print('Job\'s done.')
+print(f'Job\'s done. in {round(timer_finish - timer_start,2)} second(s)')
+
