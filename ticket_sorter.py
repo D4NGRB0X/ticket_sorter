01dd96f938ab701c3af94d43243985fe294e37b7
Thomas <thomas.meche@gmail.com>
2020-05-08 22:00:31 -0600

    5-6 Commit :     - Output .txt saves to home documents dir     - file name is printed above the output for easy human parsing

--- ticket_sorter.py
+++ ticket_sorter.py
@@ -2,17 +2,17 @@
 TODO: File pathing
     - check for xls files in dir
     - check for info match
-    - run script on all files
 TODO: Output
-    - total hours for day per client
     - label day and date
+    - error handling
 TODO: distribution
     - Package as executable
+        - pyinstaller may work
 '''
 import pandas as pd
-from pathlib import Path
+import os
 import time
-
+from pathlib import Path
 
 client = input('Please specify a client: \n')
 month = input('Please enter the month: \n')
@@ -21,10 +21,11 @@ file_path = input('Input file path: \n').strip('"')
 timer_start = time.perf_counter()
 
 file_list = Path(file_path).glob('*.xlsx')
-
-
+path = Path.home().joinpath('Documents')
+os.chdir(path)
 with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
     for file in file_list:
+        ticket_data.write(f'{file}\n\n')
         with pd.ExcelFile(file) as xls:
             days = [pd.read_excel(xls, day, parse_dates=['Start Time:', 'End Time:', 'Time Worked:'])
                     for day in range(7)]
@@ -52,12 +53,16 @@ with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
                         client_data = day['Customer'].str.contains(client.title())
                         ticket_data.write(
                             f'{client.upper()} {day.worked.sum()} \n' + day.loc[client_data].to_string(
-                                columns=['tickets'], index=False, header=False
+                                columns=['tickets', 'worked'], index=False, header=False
                             ) + '\n\n')
 
                 except AttributeError:
                     pass
+# else:
+#     print('''
+#     There has been an error. please check the following:\n
+#     Do the .xlsx files exist in the file path you have selected?
+#     ''')
 timer_finish = time.perf_counter()
 
-print(f'Job\'s done. in {round(timer_finish - timer_start,2)} second(s)')
-
+print(f'Job\'s done. in {round(timer_finish - timer_start, 2)} second(s)')
