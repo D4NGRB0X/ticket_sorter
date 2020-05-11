f4e5c428831774fee635c50b8b916a4cd5e75fe6
Thomas <thomas.meche@gmail.com>
2020-05-11 08:08:05 -0600

    5-11 Commit 2 :     - added comments     - added if __name__ == "__main__"

--- ticket_sorter.py
+++ ticket_sorter.py
@@ -27,27 +27,27 @@ path = Path.home().joinpath('Documents')
 os.chdir(path)
 
 with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
-    # Creates new text file with naming convention <Client>-<user specified date range>.txt
+    # creates new text file with naming convention <Client>-<user specified date range>.txt
     for file in file_list:
-        ticket_data.write(f'{file.stem}\n\n')  # Prints file name without path or extension
+        ticket_data.write(f'{file.stem}\n\n')  # prints file name without path or extension
         with pd.ExcelFile(file) as xls:  # creates pandas dataframe for each sheet specified in file
             num_sheets = len(xls.sheet_names)
-            if num_sheets < 7:
+            if num_sheets < 7:  # data collected on single sheet
                 days = [pd.read_excel(xls, parse_dates=['Start Time:', 'End Time:', 'Time Worked:'])]
-            else:
+            else:  # data collected on multiple sheets
                 days = [pd.read_excel(xls, day, parse_dates=['Start Time:', 'End Time:', 'Time Worked:'])
                         for day in range(7)]
 
             for day in days:
                 try:
-                    if 'Customer' in day.columns.values:
+                    if 'Customer' in day.columns.values:  # test for series header customer
                         day = day[[
                             'Start Time:',
                             'End Time:',
                             'Customer',
                             'Ticket Number/Action:',
                             'Time Worked:']].dropna()
-                    else:
+                    else:  # handle if client name not customer
                         day = day[[
                             'Start Time:',
                             'End Time:',
@@ -58,18 +58,19 @@ with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
                             'Company': 'Customer'
                         }, inplace=True)
 
+                    # rename series headers to pandas/python friendly
                     day.rename(columns={
                         'Start Time:': 'start',
                         'End Time:': 'end',
                         'Ticket Number/Action:': 'tickets',
                         'Time Worked:': 'worked'
-                    }, inplace=True)
+                    }, inplace=True)  # does swap directly
 
-                    day.worked = day.end - day.start
+                    day.worked = day.end - day.start  # calculates time worked per ticket
 
-                    if not day.empty:
-                        client_data = day['Customer'].str.contains(client.title())
-                        ticket_data.write(
+                    if not day.empty:  # ignores sheets with no data
+                        client_data = day['Customer'].str.contains(client.title())  # setup for client data
+                        ticket_data.write(  # writes column of all tickets worked for specific client
                             f'{client.upper()} {day.worked.sum()} \n\n' + day.loc[client_data].to_string(
                                 columns=['tickets', 'worked'], index=False, header=False
                             ) + '\n\n')
@@ -80,3 +81,7 @@ with open(f'{client.title()}-{month.title()}.txt', 'w') as ticket_data:
 timer_finish = time.perf_counter()
 
 print(f'Job\'s done. in {round(timer_finish - timer_start, 2)} second(s)')
+print(f'Your file will be named {client.title()}-{month.title()}.txt and be saved to {path}')
+
+if __name__ == "__main__":
+    pass
